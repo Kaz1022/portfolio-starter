a5e5c898d702b7f8b74c1b8a164f3ccb0751ffb5
Kazuko Baynton <kazukobaynton@gmail.com>
2022-08-22 12:06:19 -0700

    add darkmode state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import Navbar from "./Components/Navbar/Navbar";
 import './App.css';
 import Intro from "./Components/Intro/Intro";
@@ -5,10 +6,18 @@ import Skills from "./Components/Skills/Skills";
 import Portfolio from "./Components/Portfolio/Portfolio";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
+import {themeContext} from './Context';
 
 function App() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div className="App">
+    <div className="App"
+      style={{
+        background : darkMode? "black" : "",
+        color : darkMode? "white" : ""
+      }}    
+    >
       <Navbar />
       <Intro />
       <Skills />
