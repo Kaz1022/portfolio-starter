50253f5636a6cf6183ebfb38a7939fe4159c6bc8
Kazuko Baynton <kazukobaynton@gmail.com>
2022-09-09 14:03:53 -0700

    add new project and change layout of swiper

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -2,10 +2,13 @@ import { React, useContext } from 'react'
 import { themeContext } from '../../Context';
 import './Portfolio.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper';
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import Arpeggio from '../../img/arpeggio.png';
 import Marketplace from '../../img/marketplace.png';
 import Tweeter from '../../img/tweeter.png';
-import Tinyapp from '../../img/tinyapp.png';
+import LiamHicks from '../../img/LiamHicks.png';
 import 'swiper/css';
 
 
@@ -13,6 +16,29 @@ function Portfolio() {
 
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
+
+  const projects = [
+    {
+      id: 1,
+      img: LiamHicks,
+      github: "https://github.com/Kaz1022/liamhicks"
+    },
+    {
+      id: 2,
+      img: Arpeggio,
+      github: "https://github.com/Kaz1022/arpeggio"
+    },
+    {
+      id: 3,
+      img: Marketplace,
+      github: "https://github.com/Kaz1022/Lighthouse-Marketplace"
+    },
+    {
+      id: 4,
+      img: Tweeter,
+      github: "https://github.com/Kaz1022/tweeter-template"
+    }
+  ]
 
   return (
     <div className="portfolio" id="Portfolio">
@@ -22,35 +48,22 @@ function Portfolio() {
 
       {/* slider */}
       <Swiper
-        spaceBetween={30}
-        slidesPerView={3}
+        modules={[Pagination, Navigation]}
+        slidesPerView={1}
+        pagination={{clickable: true}}
+        navigation={{clickable: true}}
         grabCursor={true}
         className="portfolio-slider"
       >
-        <SwiperSlide>
-          <a href="https://github.com/Kaz1022/arpeggio" target="_blank" rel="noopener noreferrer">
-          <img src={Arpeggio} alt="" />
+        {projects.map((project, index) => {
+          return(
+            <SwiperSlide key={index}>
+               <a href={project.github} target="_blank" rel="noopener noreferrer">
+          <img src={project.img} alt="" />
           </a>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <a href="https://github.com/Kaz1022/Lighthouse-Marketplace" target="_blank" rel="noopener noreferrer">
-          <img src={Marketplace} alt="" />
-          </a>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <a href="https://github.com/Kaz1022/tweeter-template" target="_blank" rel="noopener noreferrer">
-          <img src={Tweeter} alt="" />
-          </a>
-        </SwiperSlide>
-
-        <SwiperSlide>
-         <a href="https://github.com/Kaz1022/tinyapp" target="_blank" rel="noopener noreferrer">
-          <img src={Tinyapp} alt="" />
-          </a>
-        </SwiperSlide>
-
+            </SwiperSlide>
+          )
+        })}
       </Swiper>
 
     </div>
